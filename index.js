61c6574866dfe62e5ecbe89a894839837b1c10f7
Shashikant <shashikantprajapti17@gmail.com>
2022-10-20 21:48:56 +0530

    fsdf

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.use('/', (req, res) => {
         message: "done"
     })
 })
+app.use('/message', (req, res) => {
+    res.send({
+        message: "Shashikant"
+    })
+})
 
 app.listen(4000, () => {
     console.log('done')
