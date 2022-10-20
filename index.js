78aacc50322a4596ea785ef863ec0e95b725358d
Shashikant <shashikantprajapti17@gmail.com>
2022-10-20 21:54:32 +0530

    sdfsdf

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send({
-        message: "done"
-    })
-})
 app.use('/message', (req, res) => {
     res.send({
         m: "Shashikant"
     })
 })
+
+app.use('/', (req, res) => {
+    res.send({
+        message: "done"
+    })
+})
+
 
 app.listen(4000, () => {
     console.log('done')
