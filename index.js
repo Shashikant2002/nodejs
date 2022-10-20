a75cbc7c83f2c7117ce17236990d807ee3edd702
Shashikant <shashikantprajapti17@gmail.com>
2022-10-20 21:45:14 +0530

    new

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => {
+    res.send({
+        message: "done"
+    })
+})
+
+app.listen(4000, () => {
+    console.log('done')
+})
